0d2571fa519ef973406286254d37c3c9bbe37447
Jacob Hauritz Bodholdt <jacobhbo@outlook.com>
2023-09-11 06:44:58 +0200

    final

---
+++ Holiday/HolidayService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic; // Tilføj dette using statement
+
+public class HolidayService
+{
+    private readonly HttpClient _httpClient;
+
+    public HolidayService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<HolidayInfo>> GetHolidaysForCountryAsync(string countryCode, int year)
+    {
+        try
+        {
+            var response = await _httpClient.GetFromJsonAsync<List<HolidayInfo>>($"https://date.nager.at/api/v3/PublicHolidays/2023/DK");
+            return response;
+        }
+        catch (Exception ex)
+        {
+            // Håndter fejl efter behov
+            Console.WriteLine($"Der opstod en fejl: {ex.Message}");
+            return null;
+        }
+    }
+
+    // Opret en klasse, der repræsenterer ferieoplysningerne
+    public class HolidayInfo
+    {
+        public DateTime date { get; set; }
+        public string name { get; set; }
+    }
+}
--- Holiday/Program.cs
+++ Holiday/Program.cs
@@ -1,28 +1,26 @@
-using Holiday.Data;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+// Configure the HTTP request pipeline here.
+builder.Services.AddSingleton<HttpClient>();
+builder.Services.AddSingleton<HolidayService>(); // Tilføj HolidayService som en singleton service.
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
 
 app.MapBlazorHub();
